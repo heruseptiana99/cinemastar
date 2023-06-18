e0f35cf33ba9c4ab3f3f8b8dd3e48620fb5e9e1c
heruseptiana99 <heruseptiana99@gmail.com>
2023-06-18 20:33:42 +0700

    heru 1.13

---
+++ model/komentar_model.php
@@ -0,0 +1,15 @@
+<?php 
+    include("../admin/connect.php");
+    $id_user = $_POST['id_user'];
+    $id_film = $_POST['id_film'];
+    $rating = $_POST['rating'];
+    $komentar = $_POST['komentar'];
+
+    mysqli_query($conn, "INSERT INTO `komentar` (`id_film`, `id_user`, `rating`, `isi_komentar`, `tgl_komentar`) VALUES ('$id_film', '$id_user', '$rating', '$komentar', ' ". date('Y-m-d H:i:s') ."');");
+
+    header("Location:../film2_detail.php?id_film=$id_film");
+
+
+
+
+?>
